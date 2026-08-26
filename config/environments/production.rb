738813b7c3523bfad35fc77fa484711b0ff20acd
Leonardo Dias <leo.bdms@gmail.com>
2026-08-26 12:02:01 -0300

    chore: update mailer configuration and add SMTP settings
    - Changed default mailer sender to "leo.bdms@gmail.com" in application_mailer.rb.
    - Added SMTP configuration variables to env.example and deploy.yml for email settings in production.
    - Updated production.rb to utilize environment variables for SMTP settings, enhancing email delivery configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV.fetch("SMTP_USER_NAME", ""),
+    password: ENV.fetch("SMTP_PASSWORD", ""),
+    address: ENV.fetch("SMTP_ADDRESS", ""),
+    host: ENV.fetch("SMTP_HOST", ""),
+    port: ENV.fetch("SMTP_PORT", ""),
+    authentication: :login
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
